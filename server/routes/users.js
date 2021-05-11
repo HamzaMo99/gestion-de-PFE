06b6e83597aff0c94ba6ddb03813eda8eec123ed
HamzaMo99 <66643425+HamzaMo99@users.noreply.github.com>
2021-05-11 02:59:28 +0200

    Delete server directory

--- server/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const usersController = require('../contollers/userConroller.js')
-const router =express.Router();
-
-
-router.post('/login',usersController.login)
-router.post('/newStudent',usersController.addUser)
-
-module.exports = router;
